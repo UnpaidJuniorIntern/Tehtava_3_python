bcf085b190f63d166de4fc578389c5488af7c172
Kristian Muthugalage <kmuthuga@edu.lapinamk.fi>
2025-10-20 11:41:46 +0300

    Muutettu user_repository tiedostoa, jossa nyt avataan tietokantayhteys

--- decorators/user_repository.py
+++ decorators/user_repository.py
@@ -4,9 +4,13 @@ from repositories.factories import create_users_repository
 
 def get_users_repository(route_handler_func):
     def wrapper(*args, **kwargs):
-        # käytetään tässä vanhaa jo käytössä olevaa create_users_repository-factorya
-        users_repository = create_users_repository()
-        # palautetaan nyt route_handler_func mukanaan users_repository
-        return route_handler_func(users_repository, *args, **kwargs)
-    # muista wrapper funktio pitää palauttaa dekoraattorin lopussa, mutta ilman sulkuja
-    return wrapper
+
+        #avataan tietokantayhteys tässä
+        with sqlite3.connect('tuntiharjoitus1.db') as con:
+
+            # annetan con-muuttuja factorylle parametrina
+            users_repository = create_users_repository(con)
+            
+            return route_handler_func(users_repository, *args, **kwargs)
+        #muista wrapper funktio pitää palauttaa dekoraattorin lopussa ilman sulkuja
+        return wrapper 
